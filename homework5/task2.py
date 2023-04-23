0717c0e14347a87590288df40ccca46cc001856c
suntsovpavel <suntsovpavel@mail.ru>
2023-04-23 16:10:21 +0300

    correct hw5, task2

--- homework5/task2.py
+++ homework5/task2.py
@@ -4,11 +4,6 @@ import random
 # описывающие случайную возрастающую последовательность. Порядок элементов менять нельзя.
 # [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [2, 7] или [4, 6, 7] и т.д.
 
-# cоздаем список, заполненный случайными числами от 1 до 10
-def createNumbers(N: int):
-    N=abs(N)
-    return [random.randint(1,10) for _ in range(N)]   
-
 # Случайным образом выбираем элементы из numbers 
 #   до тех пор, пока последовательность значений возрастает
 def getIncreasingSeries(numbers: list):  
@@ -25,8 +20,9 @@ def getIncreasingSeries(numbers: list):
             return result    
 
 def task2():
-    N = int(input('Введите число: '))
-    numbers = createNumbers(N)
+    # cоздаем список, заполненный случайными числами от 1 до 10
+    N = abs(int(input('Введите число: ')))
+    numbers = [random.randint(1,10) for _ in range(N)]   
     print(f'numbers: {numbers}')
 
     # Повторяем попытки до тех пор, пока не будет получена последовательность более чем из одного элемента
