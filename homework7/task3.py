107a5e70bb44c36e71cea6fa38d34b6f49c3dc70
suntsovpavel <suntsovpavel@mail.ru>
2023-05-28 01:54:01 +0300

    edit homework7/task3.py

--- homework7/task3.py
+++ homework7/task3.py
@@ -6,37 +6,37 @@ import random
 
 bot = telebot.TeleBot("") 
 
-isGame = False  # начата ли игра
-number = 0
+isGameStarted = False  # начата ли игра
+number = 0    # загаданное число
 countTry = 0  # количество попыток
 
 def initGame():
- global isGame, number, countTry
+ global isGameStarted, number, countTry
  countTry = 0
  number = random.randint(1,1000)    
- isGame = True 
+ isGameStarted = True 
 
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
- global isGame, number, countTry
+ global isGameStarted, number, countTry
  text = message.text.lower()
  if text == 'игра':
-  if isGame:   
+  if isGameStarted:   
    bot.reply_to(message, "Игра уже запущена. Угадайте чиcло от 1 до 1000")
   else:
    bot.reply_to(message, "Угадайте чиcло от 1 до 1000")
    initGame() 
  elif text == 'стоп':  
   bot.reply_to(message, "Игра окончена")
-  isGame = False  
+  isGameStarted = False  
  else:
-  if isGame:
+  if isGameStarted:
    if text.isdigit():
     countTry += 1
     userNumber = int(text)
     if userNumber == number:
      bot.reply_to(message, f'Поздравляем, Вы угадали число {number}! Количество попыток: {countTry}')
-     isGame = False
+     isGameStarted = False
     elif userNumber > number:
      bot.reply_to(message, f'Число {userNumber} больше загаданного')     
     elif userNumber < number:     
